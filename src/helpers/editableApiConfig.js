d0381335bf65e3a7e6039285164c2a2c8e00024c
cterryc <danyel.martel@gmail.com>
2024-01-16 06:48:06 -0500

    fix: modificado apra escuchar local y deploy

--- src/helpers/editableApiConfig.js
+++ src/helpers/editableApiConfig.js
@@ -1,6 +1,13 @@
 // export const API = 'https://formwapp.onrender.com'
-export const API = 'http://localhost:3001'
 
+const local = import.meta.env.VITE_LOCAL
+let API_BACK
+if (local) {
+  API_BACK = 'http://192.168.18.21:3001'
+} else {
+  API_BACK = import.meta.env.VITE_API
+}
+export const API = API_BACK
 // export const imagenDeRespaldo = 'https://w0.peakpx.com/wallpaper/419/206/HD-wallpaper-warcraft-darck-elf-elf.jpg'
 
 // export const urlMedia = 'https://media.rawg.io/media/crop/600/400/games'
